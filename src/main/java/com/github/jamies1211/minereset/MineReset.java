bb1b03d0a7498266bfdaa1b2c0721a76336e9bbc
JamieS1211 <jamieshepherd1211@gmail.com>
2016-10-29 23:50:42 +0100

    Bumped to 1.10.2 (API 6.0.0-SNAPSHOT)

--- src/main/java/com/github/jamies1211/minereset/MineReset.java
+++ src/main/java/com/github/jamies1211/minereset/MineReset.java
@@ -46,7 +46,7 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-@Plugin(id = "minereset", name = "MineReset", version = "1.0.6",
+@Plugin(id = "minereset", name = "MineReset", version = "1.0.7",
 		description = "Resets mines",
 		authors = {"JamieS1211"},
 		url = "http://pixelmonweb.officialtjp.com")
